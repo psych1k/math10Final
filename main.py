0f2ff10170bb5c7e8de288d9172a60820882a704
Datrags <stephan.green@scholar.democracyprep.org>
2021-08-03 14:10:41 -0400

    centered typing section

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ pygame.font.init()
 #Display Settings
 WIN_W, WIN_H = 900, 600 #game window height and width
 WIN = pygame.display.set_mode((WIN_W, WIN_H), pygame.RESIZABLE)
-FPS = 60
+FPS = 15
 
 #Fonts
 WORD_FONT100 = pygame.font.SysFont('Comic Sans', 100)
-WORD_FONT20 = pygame.font.SysFont('Comic Sans', 20)
+WORD_FONT40 = pygame.font.SysFont('Comic Sans', 40)
 #Colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 GRAY = (128,128,128)
 
 #User Input
-INPUT_RECT = pygame.Rect(WIN_W/2 - 200, WIN_H/2 + 100, 300, 100)
+#INPUT_RECT = pygame.Rect(WIN_W/2 - 70, WIN_H/2 + 100, 300, 100)
 
+#Called every frame to redraw the window every frame
 def draw_window(current_word,user_text,word_score, char_score):
     WIN.fill(BLACK)
     #Display current word
@@ -27,21 +28,19 @@ def draw_window(current_word,user_text,word_score, char_score):
         WIN_H/2 - current_word.get_height()/2))
 
     #Display Text Box
+    INPUT_RECT = pygame.Rect(WIN_W/2 - current_word.get_width()/2,
+        WIN_H/2 - current_word.get_height()/2 + 100, current_word.get_width(), 100)
     pygame.draw.rect(WIN, GRAY, INPUT_RECT)
     text_surface = WORD_FONT100.render(user_text, 1, BLACK)
     WIN.blit(text_surface, (INPUT_RECT.x+5, INPUT_RECT.y+5))
     INPUT_RECT.w = max(100, text_surface.get_width() + 10)
 
     #Display Scores
-    wscore = WORD_FONT20.render("Words Typed: "+str(word_score), 1, WHITE)
-    cscore = WORD_FONT20.render("Characters Typed: "+str(char_score), 1, WHITE)
+    wscore = WORD_FONT40.render("Words Typed: "+str(word_score), 1, WHITE)
+    cscore = WORD_FONT40.render("Characters Typed: "+str(char_score), 1, WHITE)
     WIN.blit(wscore, (10, 10))
-    WIN.blit(cscore, (10, 30))
+    WIN.blit(cscore, (10, 50))
     pygame.display.update()
-
-# def handle_typing(word, key_press):
-
-
 
 def main():
     pygame.display.set_caption("Typing Game")
@@ -65,11 +64,11 @@ def main():
                 run = False
                 pygame.quit()
                 break;
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if INPUT_RECT.collidepoint(event.pos):
-                    input_active = True
-                else:
-                    input_active = False
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     # if INPUT_RECT.collidepoint(event.pos):
+            #     #     input_active = True
+            #     # else:
+            #     #     input_active = False
 
             if event.type == pygame.KEYDOWN and event.key != pygame.K_RETURN:
                 #[:-1 removes \n]
@@ -77,7 +76,6 @@ def main():
                     user_text = user_text[:-1]
                 else:
                     user_text += event.unicode
-                print(user_text)
 
                 if user_text == word_text: #if equal to the word make a new word
                     word_score += 1
@@ -85,14 +83,6 @@ def main():
                     word_text = random.choice(diction.get_library())[:-1]
                     current_word = WORD_FONT100.render(word_text,1,WHITE)
                     user_text = ''
-                # if event.key == pygame.K_RETURN:
-                #     new_word = random.choice(diction.get_library())[:-1]
-                #     wanted_key_press = pygame.key.key_code(new_word[0])
-                #     #make a function that handles these inputs
-                #     handle_typing(new_word, event.key)
-                #     current_word = WORD_FONT100.render(new_word,1,WHITE)
-                #     if wanted_key_press == event.key:
-                #         print("YES")
         if not run:
             break
         draw_window(current_word, user_text, word_score, char_score)
--- word_library.py
+++ word_library.py
@@ -4,10 +4,6 @@ class Word_Library(object):
     def __init__(self, textfile):
         f = open(textfile,'r')
         self.library = f.readlines()
-        # for word in self.library:
-        #     word = word[:-1]
-        #     if len(word) < 4:
-        #         print(word)
         f.close()
     def return_word_range(self, start, end):
         return self.library[start:end]
